eb3d2c8d09954b5e3aebe6957faaf56184aa8788
Ghaithalmuntaser <gasa.almuntaser@student.han.nl>
2024-11-11 10:56:29 +0100

    New Branch

--- src/transfer_learning_template_train.py
+++ src/transfer_learning_template_train.py
@@ -17,7 +17,7 @@ from tytorch.utils.mlflow import set_mlflow_experiment
 from tytorch.utils.trainer_utils import get_device
 
 # get environment variables to upload artifacts to central mlflow
-load_dotenv()
+load_dotenv(dotenv_path='/Users/galmuntaser/Desktop/HAN University/hackathon/env')
 
 params = {"n_epochs": 2, "lr": 0.1}
 
